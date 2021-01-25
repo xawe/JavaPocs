ff40f208ba3cf13597ef6aa73dbf73b992166cf0
Xawe <dreamxawe@hotmail.com>
2021-01-25 19:55:10 -0300

    .

--- src/com/quickpocs/Execute.java
+++ src/com/quickpocs/Execute.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,9 +14,16 @@ import model.Foo;
 
 public class Execute {
 
+	/* 
+	 * Exemplo de chamada de multiplas funções em paralelo usando Callable e syncronizedCollection 
+	 * para armazenamento dos resultados
+	 * WIP - pendente implementação e comparação usando CopyOnWriteArrayList
+	 */
 	public static void main(String[] args) {		
 		ExecutorService executor = Executors.newWorkStealingPool();				
 		List<Callable<Foo>> callables = new ArrayList<Callable<Foo>>();
+		List<Foo> copyOnWriteArray = new CopyOnWriteArrayList<Foo>();
+		
 		long start = System.currentTimeMillis();
 		Collection<Foo> syncCollection = Collections.synchronizedCollection(new ArrayList<Foo>());
 		for (int i = 0; i < 50; i++) {
